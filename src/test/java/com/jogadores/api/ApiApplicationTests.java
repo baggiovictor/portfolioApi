8c0c55f6e08ca951cbd0681c98433354a972a56d
Victor Baggio <baggiobaggiovictor@gmail.com>
2024-10-04 15:59:03 -0300

    (feat): First commit

---
+++ src/test/java/com/jogadores/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jogadores.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
